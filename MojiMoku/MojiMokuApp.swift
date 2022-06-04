d7c995cda908a7ef692b2e501b72a494b6d6c8c4
Vanness Zhong Anthony <vannessanthony123@gmail.com>
2022-06-04 14:59:15 +0700

    Initial Commit

---
+++ MojiMoku/MojiMokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MojiMokuApp.swift
+//  MojiMoku
+//
+//  Created by Vanness Anthony on 04/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MojiMokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
